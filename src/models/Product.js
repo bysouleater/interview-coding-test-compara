6f10f057e3ad11b31ea87d27de94c50e57397ed0
Christian Nahuel Balsamo <bysouleater@gmail.com>
2019-09-08 19:13:15 -0300

    Normal Products Logic

--- src/models/Product.js
+++ src/models/Product.js
@@ -4,6 +4,19 @@ class Product {
     this.sellIn = sellIn;
     this.price = price;
   }
+
+  updateProduct() {
+    this.updateSellIn();
+    this.updatePrice();
+  }
+
+  updateSellIn() {
+    this.sellIn -= 1;
+  }
+
+  updatePrice() {
+    this.price = Math.max(0, this.price - (this.sellIn >= 0 ? 1 : 2));
+  }
 }
 
 module.exports = Product;
