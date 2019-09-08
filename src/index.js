637b7e4baa4c248439a94f749c44db4802072c04
BySouleater <bysouleater@gmail.com>
2019-09-08 19:47:47 -0300

    Legendary Product (#5)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 const Product = require('./models/Product');
 const IncreasingPriceProduct = require('./models/IncreasingPriceProduct');
 const CarInsurance = require('./models/CarInsurance');
+const LegendaryProduct = require('./models/LegendaryProduct');
 
 const productsAtDayZero = [
   new Product('Medium Coverage', 10, 20),
   new IncreasingPriceProduct('Full Coverage', 2, 0),
   new Product('Low Coverage', 5, 7),
-  new Product('Mega Coverage', 0, 80),
-  new Product('Mega Coverage', -1, 80),
+  new LegendaryProduct('Mega Coverage', 0, 80),
+  new LegendaryProduct('Mega Coverage', -1, 80),
   new Product('Special Full Coverage', 15, 20),
   new Product('Special Full Coverage', 10, 49),
   new Product('Special Full Coverage', 5, 49),
